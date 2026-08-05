4c7793e0c06437f3f5a25bb5677ea10af37d5ef7
Quan Huynh <61860115+warm-oats@users.noreply.github.com>
2026-08-05 20:42:32 +0000

    Add unit test coverage

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -23,5 +23,26 @@ require 'rails_helper'
 # You should add your own test cases.
 # We recommend creating a file for each model in the database.
 
-# RSpec.describe Representative do
-# end
+RSpec.describe Representative do
+  describe ".civic_api_to_representative_params" do
+    before(:each) do
+      @rep_res = Representative.create({ name: "Donald Beyer", ocdid: "412345",
+      title: "representative", party: "Democrat", photo_url: "https://www.congress.gov/img/member/b001292_200.jpg" })
+
+      allow(Representative).to receive(:find_rep).and_return(@rep_res)
+      @rep_info = JSON.parse(File.read("./spec/api_json_dump.json"))
+    end
+    it "should return array containing rep objects" do
+      result = Representative.civic_api_to_representative_params(@rep_info)
+
+      expect(result).to eq([@rep_res])
+    end
+    it "should return empty array when given no data" do
+      result = Representative.civic_api_to_representative_params([])
+
+      expect(result).to eq([])
+    end
+    it "should call .find_rep with the right arguments" do
+    end
+  end
+end
