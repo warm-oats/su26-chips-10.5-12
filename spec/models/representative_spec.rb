20248266b76279b364eb857d7e8d9987ef43adc8
Quan Huynh <61860115+warm-oats@users.noreply.github.com>
2026-08-06 04:55:24 +0000

    Add test coverage, fix method bug db dup and return wrong object
    Method now returns a rep object instead of just true or false with .save. It also shouldn't save a rep object if it already exists in db. .create both creates and save a rep object.

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -25,15 +25,11 @@
 #
 require 'rails_helper'
 
-# This file is a stub.
-# You should add your own test cases.
-# We recommend creating a file for each model in the database.
-
 RSpec.describe Representative do
   describe '.civic_api_to_representative_params' do
     before do
-      @rep_res = described_class.create({ name: 'Donald Beyer', ocdid: '412657',
-      title: 'representative' })
+      @rep_res = described_class.new(name: 'Donald Beyer', ocdid: '412657',
+                                     title: 'representative')
 
       @rep_info = JSON.parse(File.read('spec/geocodio_api_call_dump.json'))
       response = @rep_info['results'][0]['response']['results'][0]['fields']
@@ -66,6 +62,45 @@ RSpec.describe Representative do
 
     it 'returns non-empty arr with valid args' do
       expect(@result).not_to be_empty
+    end
+  end
+
+  describe '.find_rep' do
+    def find_rep
+      described_class.find_rep(@official, title: 'representative', ocdid: '412657')
+    end
+
+    before do
+      @rep_info = JSON.parse(File.read('spec/geocodio_api_call_dump.json'))
+      @official = @rep_info['results'][0]['fields']['congressional_districts'][0]['current_legislators'][0]
+      @official['name'] = "#{@official.dig('bio', 'first_name')} #{@official.dig('bio', 'last_name')}"
+    end
+
+    it 'raises ArgumentError for invalid args' do
+      expect { described_class.find_rep(nil) }.to raise_error(ArgumentError)
+      expect { described_class.find_rep(nil, 'Donald Beyer', '123456') }.to raise_error(ArgumentError)
+      expect { described_class.find_rep('12345', @official, 'Donald Beyer') }.to raise_error(ArgumentError)
+      expect { described_class.find_rep }.to raise_error(ArgumentError)
+    end
+
+    it 'saves rep if rep not in db' do
+      expect { find_rep }
+        .to change(described_class, :count).from(0).to(1)
+    end
+
+    it 'does not save duplicate reps' do
+      expect { find_rep }
+        .to change(described_class, :count).from(0).to(1)
+      expect { find_rep }
+        .not_to change(described_class, :count)
+    end
+
+    it 'returns correct rep object with properties' do
+      rep = find_rep
+
+      expect(rep.name).to eq('Donald Beyer')
+      expect(rep.ocdid).to eq('412657')
+      expect(rep.title).to eq('representative')
     end
   end
 end
